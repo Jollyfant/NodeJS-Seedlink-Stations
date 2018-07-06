e395e298fe4c76c4addcfb39deadb219cc671395
Jollyfant <koymans@knmi.nl>
2018-07-06 13:08:40 +0200

    Full refactor; add Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,26 @@
-# Dockerfile for building Seedlink station connector
+# Dockerfile for building NodeJS Seedlink Station connector
 #
+# Build the container:
 # $ docker build -t seedlink-stations:1.0 .
+#
+# And run the container (may omit the -e flags):
+# $ docker run --rm -p 8086:8086 -e "SERVICE_PORT=8087" -e "SERVICE_HOST=0.0.0.0" seedlink-stations:1.0
 
 FROM node:8
-MAINTAINER Mathijs Koymans
+
+# Add metadata
+LABEL maintainer="Mathijs Koymans"
+LABEL email="koymans@knmi.nl"
 
 # Set the work directory
 WORKDIR /usr/src/app
 
 # Copy the source code
 COPY . .
+
+# Set default environment variables
+ENV SERVICE_HOST=0.0.0.0
+ENV SERVICE_PORT=8086
 
 EXPOSE 8086
 
--- index.js
+++ index.js
@@ -1,27 +1,170 @@
-/* NodeJS-Seedlink-Stations
+/*
+ * nodejs-seedlink-stations-proxy
  *
  * NodeJS application for reading the available 
  * stations from arbitrary Seedlink servers.
  *
- * Copyright (c) Mathijs Koymans, 2018
- * Licensed under MIT.
- *
+ * Copyright ORFEUS Data Center
+ * Author: Mathijs Koymans
+ * Licensed under MIT
  *
  */
 
-const Network = require("net");
-const Http = require("http");
-const CONFIG = require("./config");
+const network = require("net");
+const http = require("http");
 const url = require("url");
 const querystring = require("querystring");
+const path = require("path");
+const fs = require("fs");
 
 // Global container for Stations
-var GLOBAL_STATIONS = new Object(); 
-const CAT_NOT_IMPLEMENTED = 232;
+var SeedlinkStationProxy = function(configuration, callback) {
 
-function validateAllowed(key) {
+  /* class SeedlinkStationProxy
+   * NodeJS proxy for getting Seedlink station information
+   */
 
-  /* function validateAllowed
+  function HTTPError(response, statusCode, message) {
+ 
+    /* function HTTPError
+     * Writes HTTP reponse to the client
+     */
+
+    response.writeHead(statusCode, {"Content-Type": "text/plain"});
+    response.write(message);
+    response.end();
+
+  }
+
+  function EnableCORS(response) {
+
+    /* function EnableCORS
+     * Enables the cross origin headers
+     */
+
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "GET");
+
+  }
+
+  this.configuration = configuration;
+  this.logger = this.setupLogger();
+
+  this.cachedStations = new Object();
+
+  // Create the HTTP server
+  const Server = http.createServer(function(request, response) {
+
+    // Enable CORS headers when required
+    if(this.configuration.__CORS__) {
+      EnableCORS(response);
+    }
+
+    var uri = url.parse(request.url);
+    var initialized = Date.now();
+
+    // Only root path is supported
+    if(uri.pathname !== "/") {
+      return HTTPError(response, 404, "Method not supported.")
+    }
+
+    var queryObject = querystring.parse(uri.query);
+
+    if(!queryObject.host) {
+      return HTTPError(response, 400, "Host parameter is required.");
+    }
+
+    // Get the comma delimited of requested servers 
+    servers = queryObject.host.split(",").map(this.parseHost);
+
+    if(servers.length === 0) {
+      return HTTPError(response, 204);
+    }
+
+    // Check user input
+    try {
+      Object.keys(queryObject).forEach(this.validateParameters);
+      servers.forEach(this.validateServer);
+    } catch(exception) {
+      if(this.configuration.__DEBUG__) {
+        return HTTPError(response, 400, exception.stack);
+      } else {
+        return HTTPError(response, 400, exception.message);
+      }
+    }
+
+    // Write information to logfile
+    response.on("finish", function() {
+      this.logger.write(JSON.stringify({
+        "timestamp": new Date().toISOString(),
+        "method": request.method,
+        "query": uri.query,
+        "path": uri.pathname,
+        "client": request.headers["x-forwarded-for"] || request.connection.remoteAddress,
+        "agent": request.headers["user-agent"] || null,
+        "statusCode": response.statusCode,
+        "type": "HTTP Request",
+        "msRequestTime": (Date.now() - initialized)
+      }) + "\n");
+    }.bind(this));
+
+    // Make queries to the servers (or read from cache)
+    this.readSeedlinkStations(servers, function(data) {
+
+      response.writeHead(200, {"Content-Type": "application/json"});
+      response.write(JSON.stringify(data));
+      response.end();
+
+    });
+
+  }.bind(this));
+
+  // Get process environment variables (Docker)
+  var host = process.env.SERVICE_HOST || this.configuration.HOST;
+  var port = Number(process.env.SERVICE_PORT) || this.configuration.PORT;
+
+  // Listen to incoming HTTP connections
+  Server.listen(this.configuration.PORT, this.configuration.HOST, function() {
+    callback(configuration.__NAME__, host, port); 
+  }); 
+
+}
+
+SeedlinkStationProxy.prototype.parseHost = function(x) {
+
+  /* function SeedlinkStationProxy.parseHost
+   * Parses host:port to usable object
+   */
+
+  const DEFAULT_SEEDLINK_PORT = 18000;
+
+  var [host, port] = x.split(":");
+
+  port = Number(port) || DEFAULT_SEEDLINK_PORT;
+
+  return {
+    "url": host + ":" + port,
+    "host": host,
+    "port": port
+  }
+
+}
+
+SeedlinkStationProxy.prototype.setupLogger = function() {
+
+  /* function SeedlinkStationProxy.setupLogger
+   * Sets up log directory and file for logging
+   */
+
+  // Create the log directory if it does not exist
+  fs.existsSync(path.join(__dirname, "logs")) || fs.mkdirSync(path.join(__dirname, "logs"));
+  return fs.createWriteStream(path.join(__dirname, "logs", "service.log"), {"flags": "a"});
+
+}
+
+SeedlinkStationProxy.prototype.validateParameters = function(key) {
+
+  /* function SeedlinkStationProxy.validateParameters
    * Validates whether a key is allowed;
    */
 
@@ -29,139 +172,113 @@ function validateAllowed(key) {
     "host"
   ];
 
-  if(ALLOWED_PARAMETERS.indexOf(key) === -1) {
-    throw("Key " + key + " is not supported");
+  if(!ALLOWED_PARAMETERS.includes(key)) {
+    throw new Error("Key " + key + " is not supported.");
   }
 
 }
 
-function validateParameters(queryObject) {
+SeedlinkStationProxy.prototype.validateServer = function(server) {
 
-  if(queryObject.port < 0 || queryObject.port >= (1 << 16)) {
-    throw("A submitted port is invalid");
+  /* function SeedlinkStationProxy.validateServer
+   * Validates the submitted server port
+   */
+
+  // Check if port is integer
+  if(!Number.isInteger(server.port)) {
+    throw new Error("A submitted port is non-numerical.");
+  }
+
+  // Check the submitted port
+  if(server.port < 0 || server.port >= (1 << 16)) {
+    throw new Error("A submitted port is outside the valid range.");
   }
 
 }
 
-module.exports = function(callback) {
+SeedlinkStationProxy.prototype.readSeedlinkStations = function(servers, callback) {
 
-  function HTTPError(response, statusCode, message) {
+  /* function SeedlinkStationProxy.readSeedlinkStations
+   * Concurrently but asynchronously updates the cache 
+   */
 
-    /* function HTTPError
-     * Returns an HTTP error to client
-     */
-
-    response.writeHead(statusCode, {"Content-Type": "text/plain"});
-    response.end(message)
-  
-  }
-
-  // Create a HTTP server
-  const Server = Http.createServer(function(request, response) {
-
-    // Sets CORS headers
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Methods", "GET");
-
-    var uri = url.parse(request.url);
-    var queryObject = querystring.parse(uri.query);
-
-    // Check if a query is submitted
-    if(uri.query === null || uri.query === "") {
-      return HTTPError(response, 400, "Empty query string submitted");
-    }
-
-    if(!Object.prototype.hasOwnProperty.call(queryObject, "host")) {
-      return HTTPError(response, 400, "Host parameter is required");
-    }
-
-    // Only root path is supported
-    if(uri.pathname !== "/") {
-      return HTTPError(response, 405, "Method not supported")
-    }
-
-    // Get the comma delimited host:port values
-    servers = queryObject.host.split(",").map(function(x) {
-      var [host, port] = x.split(":"); 
-      return {
-        "url": x,
-        "host": host,
-        "port": port || 18000
-      }
-    });
-
-    // Check user input
-    try {
-      Object.keys(queryObject).forEach(validateAllowed);
-      servers.forEach(validateParameters);
-    } catch(exception) {
-      return HTTPError(response, 400, exception);
-    }
-
-    // Make queries to the servers (or read from cache)
-    updateAll(servers, function(data) {
-      response.end(JSON.stringify(data));
-    });
-
-  });
-
-  // Listen to incoming HTTP connections
-  Server.listen(CONFIG.PORT, CONFIG.HOST, function() {
-    if(typeof callback === "function") {
-      callback();
-    }
-  });
-
-}
-
-// Start the NodeJS Seedlink Server
-if(require.main === module) {
-
-  // Start up the WFCatalog
-  new module.exports(function() {
-    console.log("NodeJS Latency Server has been initialized on " + CONFIG.HOST + ":" + CONFIG.PORT)
-  });
-
-}
-
-function updateAll(servers, callback) {
-
+  // Collect the results for all servers
   var results = new Array();
 
   // Asynchronously but concurrently get the data
   (next = function() {
-    SeedlinkChecker(servers.pop(), function(result) {
-      results.push(result);
-      if(!servers.length) {
-        return callback(results);
+
+    var server = servers.pop();
+
+    this.checkSeedlink(server, function(result) {
+
+      // Update the information in the hashMap
+      if(result.error === null) {
+        this.cachedStations[server.url] = result;
       }
-      next();
-    });
-  })();
+
+      results.push(result);
+
+      // Proceed to the next requested server
+      if(servers.length) {
+        return next();
+      }
+
+      callback(results);
+
+    }.bind(this));
+
+  }.bind(this))();
 
 }
 
-function isCached(host) {
+SeedlinkStationProxy.prototype.isCached = function(host) {
 
   /* function isCached
    * Returns boolean whether a seedlink server is cached
    */
 
-  return GLOBAL_STATIONS.hasOwnProperty(host) && GLOBAL_STATIONS[host].requested > (Date.now() - CONFIG.REFRESH_INTERVAL);
+  var cachedValue = this.cachedStations[host];
+
+  return cachedValue.requested > (Date.now() - this.configuration.REFRESH_INTERVAL_MS);
 
 }
 
-function SeedlinkChecker(server, callback) {
+SeedlinkStationProxy.prototype.newRequestData = function(server) {
 
-  function finish(socket, host, data, callback) {
+  /* function SeedlinkStationProxy.newRequestData
+   * Returns new default object for request metadata/data
+   */
+
+  return {
+    "server": server,
+    "stations": new Array(),
+    "error": null,
+    "version": null,
+    "identifier": null,
+    "connected": false,
+    "requested": Date.now()
+  }
+
+}
+
+SeedlinkStationProxy.prototype.checkSeedlink = function(server, callback) {
+
+  var finish = function(socket, host, callback, requestData) {
   
-    GLOBAL_STATIONS[host] = data;
+    /* function finish
+     * Function to be bound and called per request
+     */
+
+    // Destroy the socket
     socket.destroy();
-    callback(data);
+
+    // Callback with particular request data
+    callback(requestData);
   
   }
 
-  /* Function SeedlinkChecker
+  /* Function checkSeedlink
    * Checks if Seedlink is present
    * Returns all metadata: networks, stations, sites
    */
@@ -171,42 +288,25 @@ function SeedlinkChecker(server, callback) {
   const CAT_COMMAND = "CAT" + CRNL;
   const HELLO_COMMAND = "HELLO" + CRNL;
 
+  // Extract the Seedlink server URL
   var url = server.url;
 
-  // If the host is in the cache
-  if(isCached(url)) {
-    return callback(GLOBAL_STATIONS[url]);
+  // If the host is still in the cache
+  if(this.cachedStations.hasOwnProperty(url) && this.isCached(url)) {
+    return callback(this.cachedStations[url]);
   }
 
-  // Metadata for the request
-  var requestData = {
-    "host": url,
-    "stations": new Array(),
-    "error": null,
-    "version": null,
-    "identifier": null,
-    "connected": false,
-    "requested": Date.now()
-  }
+  // Metadata for the request to be filled
+  var requestData = this.newRequestData(server);
 
   // Create a new TCP socket and empty buffer
-  var socket = new Network.Socket()
+  const socket = new network.Socket()
   var buffer = new Buffer(0);
 
-  // Set Timout in milliseconds
-  socket.setTimeout(CONFIG.SOCKET.TIMEOUT);
+  // Bind some parameters to the finish function
+  finish = finish.bind(this, socket, url, callback);
 
-  socket.on("error", function() {
-    requestData.error = "ECONNREFUSED";
-    finish(socket, url, requestData, callback);
-  });
-
-  socket.on("timeout", function() {
-    requestData.error = "ECONNREFUSED";
-    finish(socket, url, requestData, callback);
-  });
-
-  // When the connection is established write HELLO
+  // When the connection is established write HELLO to Seedlink
   socket.connect(server.port, server.host, function() {
     socket.write(HELLO_COMMAND);
   });
@@ -231,7 +331,7 @@ function SeedlinkChecker(server, callback) {
       // Reset the buffer for the next request
       buffer = new Buffer(0);
 
-      // Proceed with the CAT command
+      // Info was requested: proceed with the CAT command
       return socket.write(CAT_COMMAND);
 
     }
@@ -239,35 +339,70 @@ function SeedlinkChecker(server, callback) {
     // If the command was not implemented (e.g. IRIS ringserver)
     if(buffer.toString() === "CAT command not implemented" + CRNL) {
       requestData.error = "CATNOTIMPLEMENTED";
-      finish(socket, url, requestData, callback);
+      finish(requestData);
     }
 
-    // End of the response
+    // End of the Seedlink response
     if(buffer.lastIndexOf("\nEND") === buffer.length - 4) {
-      requestData.stations = parseBuffer(buffer);
-      finish(socket, url, requestData, callback);
+      requestData.stations = this.parseBuffer(buffer);
+      finish(requestData);
     }
 
+  }.bind(this));
+
+  // An error occured connecting to Seedlink
+  socket.on("error", function() {
+    requestData.error = "ECONNREFUSED";
+    finish(requestData);
   });
+
+  // Timeout occured connecting to Seedlink
+  socket.on("timeout", function() {
+    requestData.error = "ECONNREFUSED";
+    finish(requestData);
+  });
+
+  // Set Timout in milliseconds
+  socket.setTimeout(this.configuration.SOCKET.TIMEOUT);
 
 }
 
-function parseBuffer(buffer) {
+SeedlinkStationProxy.prototype.parseBuffer = function(buffer) {
 
-  /* function parseBuffer
+  /* function SeedlinkStationProxy.parseBuffer
    * Extracts network, station information from Seedlink CAT response
    */
 
-  // Cut off the END
-  buffer = buffer.slice(0, buffer.lastIndexOf("\nEND"));
-
   // Split by line and map result
-  return buffer.toString().split("\n").map(function(x) {
-    return {
-      "network": x.slice(0, 2).trim(),
-      "station": x.slice(3, 8).trim(),
-      "site": x.slice(9, x.length).trim()
-    }
+  return buffer.slice(0, buffer.lastIndexOf("\nEND")).toString().split("\n").map(this.parseResponse);
+
+}
+
+SeedlinkStationProxy.prototype.parseResponse = function(x) {
+
+  /* function SeedlinkStationProxy.parseResponse
+   * Extracts network, station, site from the Seedlink response
+   */
+
+  return {
+    "network": x.slice(0, 2).trim(),
+    "station": x.slice(3, 8).trim(),
+    "site": x.slice(9, x.length).trim()
+  }
+
+}
+
+// Expose the class
+module.exports = SeedlinkStationProxy;
+
+// Start the NodeJS Seedlink Server
+if(require.main === module) {
+
+  const CONFIG = require("./config");
+
+  // Start up the WFCatalog
+  new module.exports(CONFIG, function(name, host, port) {
+    console.log(name + " microservice has been started on " + host + ":" + port);
   });
 
 }
